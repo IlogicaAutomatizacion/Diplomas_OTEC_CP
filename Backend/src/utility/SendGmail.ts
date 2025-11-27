11c0f0116010030d9d876b05fe1f62fcbc200ff9
NightReversedFold <angel74977@gmail.com>
2025-11-26 18:49:22 -0600

    fgm

--- Backend/src/utility/SendGmail.ts
+++ Backend/src/utility/SendGmail.ts
@@ -1,77 +1,52 @@
-import { client_id, client_secret, refresh_token } from "../vars";
-import { google } from "googleapis";
 
-interface Params {
-    msgHtml: string;
-    para: string;
-    subject: string;
-    pdfBuffer?: Buffer;
-    nombre?: string;
+import { client_id, client_secret, refresh_token } from "../vars";
+
+async function obtenerAccessToken() {
+
+    const res = await fetch("https://oauth2.googleapis.com/token", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams({
+            client_id: client_id,
+            client_secret: client_secret,
+            refresh_token: refresh_token,
+            grant_type: "refresh_token"
+        })
+    });
+    const data: any = await res.json();
+
+    console.log(data)
+    return data.access_token;
 }
 
-export default async (msgHtml:string, para:string, subject:string, pdfBuffer?:Buffer, nombre?:string) => {
+export default async (msg: string, para: string, subject: string) => {
+    const accesToken = await obtenerAccessToken()
 
-    const oauth2Client = new google.auth.OAuth2(
-        client_id,
-        client_secret,
-        "https://developers.google.com/oauthplayground"
-    );
+    const subjectEncoded = `=?UTF-8?B?${Buffer.from(subject, "utf8").toString("base64")}?=`;
 
-    oauth2Client.setCredentials({
-        refresh_token: refresh_token,
-    });
-
-    const gmail = google.gmail({ version: "v1", auth: oauth2Client });
-
-    // --------- ENCODE BUFFERS PARA QUE NO SE CORROMPAN ----------
-    const base64Url = (str: Buffer | string) =>
-        Buffer.from(str)
-            .toString("base64")
-            .replace(/\+/g, "-")
-            .replace(/\//g, "_")
-            .replace(/=+$/, "");
-
-    // --------- ESTRUCTURA MIME DEL EMAIL ----------
-    let mime = [
-        `From: "Automatizacion ConeXion Process" <automatizacion@ilogica-soluciones.cl>`,
+    const mensaje = [
+        "From: Automatizacion ConeXion Process <automatizacion@ilogica-soluciones.cl>",
         `To: ${para}`,
-        `Subject: =?utf-8?B?${Buffer.from(subject).toString("base64")}?=`, // permite tildes
-        `MIME-Version: 1.0`,
-    ];
+        `Subject: ${subjectEncoded}`,
+        'Content-Type: text/html; charset="UTF-8"',
+        "Content-Transfer-Encoding: 8bit",
+        "",
+        `${msg}`
+    ].join("\n");
 
-    if (!pdfBuffer) {
-        // ------------ SIN ADJUNTOS --------------
-        mime.push(`Content-Type: text/html; charset="UTF-8"\n`);
-        mime.push(msgHtml);
-    } else {
-        // ------------ CON PDF ADJUNTO --------------
-        const boundary = "----=_Part_" + Math.random().toString(36).substring(2);
+    const raw = Buffer.from(mensaje, "utf-8")
+        .toString("base64") 
+        .replace(/\+/g, "-")
+        .replace(/\//g, "_")
+        .replace(/=+$/, "");
 
-        mime.push(`Content-Type: multipart/mixed; boundary="${boundary}"\n`);
-        mime.push(`--${boundary}`);
-        mime.push(`Content-Type: text/html; charset="UTF-8"\n`);
-        mime.push(msgHtml);
 
-        mime.push(`\n--${boundary}`);
-        mime.push(
-            `Content-Type: application/pdf; name="${nombre}.pdf"`
-        );
-        mime.push(`Content-Disposition: attachment; filename="${nombre}.pdf"`);
-        mime.push(`Content-Transfer-Encoding: base64\n`);
-        mime.push(pdfBuffer.toString("base64"));
-
-        mime.push(`--${boundary}--`);
-    }
-
-    const rawMessage = base64Url(mime.join("\n"));
-
-    // ---------- ENVIAR ----------
-    const res = await gmail.users.messages.send({
-        userId: "me",
-        requestBody: {
-            raw: rawMessage
-        }
+    const res = await fetch("https://gmail.googleapis.com/gmail/v1/users/me/messages/send", {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${accesToken}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ raw })
     });
-
-    return res.data;
 }
