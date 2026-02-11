a3f12b82320974af50b46fc45344441932316a84
NightReversedFold <angel74977@gmail.com>
2026-02-11 00:38:37 -0600

    fsdaafsdx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createHashRouter, RouterProvider } from 'react-router-dom'
 
 //import Certificado from './templates/Certificados/CP/Certificado'
 import PanelAdministrador from './templates/PanelAdministrador/PanelAdministrador'
@@ -62,7 +62,7 @@ window.fetch = async (
   return res;
 };
 
-const router = createBrowserRouter([
+const router = createHashRouter([
 
   {
     errorElement: <ErrorPage />,
