c8c8611ded0d518e67f79d7e901feadf783a9bb4
NightReversedFold <angel74977@gmail.com>
2026-02-10 19:15:09 -0600

    ggcfvb

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default ({ mode }: { mode: string }) => {
-  const env = loadEnv(mode, process.cwd(), '')
 
   return defineConfig({
 
