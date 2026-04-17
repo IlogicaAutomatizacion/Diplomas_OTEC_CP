db1eb3f69f89ae809422afecadf298a7ef3f8cf1
NightReversedFold <angel74977@gmail.com>
2026-04-17 16:55:13 -0600

    fix encuestas publicas

--- src/templates/Encuestas/EncuestasSatisfaccion/Usuario.tsx
+++ src/templates/Encuestas/EncuestasSatisfaccion/Usuario.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react"
 import ResponderEncuesta from "../FormatoBaseEncuesta"
 import { useParams } from "react-router-dom"
-import { obtenerFormatoEncuestaSatisfaccionUsuarioAsync, obtenerRespuestasEncuestaAsync } from "../../PanelAdministrador/Api/formatos-dinamicos"
+import { obtenerFormatoEncuestaSatisfaccionUsuarioAsync } from "../../PanelAdministrador/Api/formatos-dinamicos"
 import type { CrearRespuestaEncuestaDto, EncuestaFormato } from "../../PanelAdministrador/Componentes/CreadorDeEncuesta"
-import { agregarRespuestaEncuestaAsync } from "../../PanelAdministrador/Api/inscripciones"
+import { agregarRespuestaEncuestaAsync, obtenerRespuestasEncuestaAsync } from "../../PanelAdministrador/Api/inscripciones"
 
 export default () => {
     const { token_inscripcion, token_suscriptor } = useParams<{
